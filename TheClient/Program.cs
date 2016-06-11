bc403abca7511d313e9a9f6475429e0c940d71cf
George Chakhidze <0xfeeddeadbeef@gmail.com>
2016-06-11 22:09:03 +0400

    Initial commit

---
+++ TheClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new ServiceReference1.Service1Client())
+            {
+                var rsp = client.GetDataUsingDataContract(new ServiceReference1.CompositeType
+                {
+                    BoolValue = true,
+                    StringValue = "FUBAR"
+                });
+
+                Console.Out.WriteLine(rsp.StringValue);
+            }
+        }
+    }
+}
---
+++ TheService/IService1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TheService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract(ProtectionLevel = System.Net.Security.ProtectionLevel.Sign)]
+        string GetData(int value);
+
+        [OperationContract(ProtectionLevel = System.Net.Security.ProtectionLevel.Sign)]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
